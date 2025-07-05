23b90e81f50b2d75b4dabae246f01610a0b04e90
Mahamudul Hasan <mahamudulhasanmonir05@gmail.com>
2025-07-05 23:15:02 +0600

    PHP Constants

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//Constants
+
+// define('STATUS_PAID', 'paid');
+
+// echo defined('STATUS_PAID');
+
+// const STATUS_PAID = 'paid';
+
+// echo STATUS_PAID;
+
+// if(true) {
+//     const FOO = 'bar';
+//     define('STATUS_PAID', 9);
+// }
+
+// $paid = 'PAID';
+
+// define('STATUS_' . $paid, $paid);
+
+// echo STATUS_PAID;
+
+//Predefined constants
+
+$foo = 'bar';
+
+$$foo = 'baz';
+
+echo "$foo, {$$foo}";
